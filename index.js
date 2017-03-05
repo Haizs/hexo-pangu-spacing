b737ac84ee48c3dc6aaf7f6d4ed25aa9a93f56e8
Haizs <Jmas2711@gmail.com>
2017-03-05 11:13:25 +0800

    Handle before render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var pangu = require('pangu');
 
-hexo.extend.filter.register('after_post_render', function(data) {
+hexo.extend.filter.register('before_post_render', function(data) {
   data.title = pangu.spacing(data.title);
   data.content = pangu.spacing(data.content);
 });
